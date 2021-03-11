6a3c015a805306330f65437241d081615ca91051
Bhabadyuti <bbal@sii.pl>
2021-03-11 17:23:25 +0530

    first commit

---
+++ src/test/java/com/bb/app/SpringUnitTestingWithJunitAndMockitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bb.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringUnitTestingWithJunitAndMockitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
